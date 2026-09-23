493500c39589b3f55f4e18c5c9a0de8115dcc4e8
egermanas34-beep <egermanas34@gmail.com>
2026-09-23 12:24:29 +0300

    pridejau utf8 koduote

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <windows.h>
 
 int main() {
-
+SetConsoleOutputCP(CP_UTF8); // Nustatome konsolės išvesties koduotę į UTF-8
+    SetConsoleCP(CP_UTF8); // Nustatome konsolės įvesties koduotę į UTF-8
 
     return 0;
 }
