e21f9473e46003ccaf5b7ccf09ddda4ba9389368
egermanas34-beep <egermanas34@gmail.com>
2026-09-23 12:29:19 +0300

    pridejau ka naudosiu siame darbe

--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <string>
 #include <windows.h>
+using std::string;
+
+using std::isalpha;
+using std::tolower;
+using std::ifstream;
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::ispunct;
 
 int main() {
 SetConsoleOutputCP(CP_UTF8); // Nustatome konsolės išvesties koduotę į UTF-8
     SetConsoleCP(CP_UTF8); // Nustatome konsolės įvesties koduotę į UTF-8
+
+
+
 
     return 0;
 }
